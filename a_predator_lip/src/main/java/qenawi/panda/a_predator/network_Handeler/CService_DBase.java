a733b6045ae2ecb9f8cf2c3dd4bbaa710a96acf7
Xseal <123123>
2019-01-04 04:40:33 +0200

    fix static abstract  ));

--- a_predator_lip/src/main/java/qenawi/panda/a_predator/network_Handeler/CService_DBase.java
+++ a_predator_lip/src/main/java/qenawi/panda/a_predator/network_Handeler/CService_DBase.java
@@ -1,9 +1,9 @@
 package qenawi.panda.a_predator.network_Handeler;
 
-
 public abstract class CService_DBase
 {
-    boolean Is_Data_Good() {
+    public   boolean Is_Data_Good()
+    {
         return false;
-    }
+    };
 }
